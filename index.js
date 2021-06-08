656390d827fc91b1b99d5630d3352847f3dba5e7
Jesstern Rays <jsstrn@users.noreply.github.com>
2021-06-09 02:28:18 +0800

    Use channel name instead of channel id

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { telegram } = require('./src/bot')
 const { getJoke } = require('./src/jokes')
-const { telegramChannelName, telegramChannelId } = require('./src/constants')
+const { telegramChannelName } = require('./src/constants')
 
 module.exports.run = async (event, context, callback) => {
   try {
     const joke = await getJoke()
     const message = `${joke}\n\n➡️  ${telegramChannelName}`
 
-    await telegram.sendMessage(telegramChannelId, message)
+    await telegram.sendMessage(telegramChannelName, message)
 
     return callback(null, {
       statusCode: 200,
